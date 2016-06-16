166e312471f40f950947b2e942ef8dfc19a8f6df
Polar-Beard <saramontecino@outlook.com>
2016-06-16 12:47:04 -0700

    Add fake data, fix Gradle issue.

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -8,6 +8,9 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -57,6 +60,17 @@ public class MainActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            ArrayList<String> fakeData = new ArrayList<String>();
+            fakeData.add("Today - Sunny - 88/63");
+            fakeData.add("Tomorrow - Foggy - 70/46");
+            fakeData.add("Weds - Cloudy - 72/63");
+            fakeData.add("Thurs - Rainy - 64/51");
+            fakeData.add("Fri - Foggy - 70/46");
+            fakeData.add("Sat - Sunny - 76/68");
+
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, fakeData);
+
             return rootView;
         }
     }
